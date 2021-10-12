9c9de28a0f4818a0b7997cc79ed0e8bc36a23e48
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-12 06:42:47 +0530

    feat: add function to calculate the change

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const cashAmount = document.querySelector("#cash-amount");
 const checkButton = document.querySelector("#check-button");
 
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+var notes = [2000,500,100,20,10,5,1];
 
 checkButton.addEventListener("click",()=>{
     hideMessage();
@@ -29,5 +32,9 @@ function showMessage(msg){
 }
 
 function calculateChange(amountToReturn){
-    console.log(amountToReturn);
+    for(let i=0 ; i<notes.length ; i++){
+        const numberOfNotes = Math.trunc(amountToReturn/notes[i]);
+        amountToReturn %= notes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
