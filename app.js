8d5a1d36198834aafba760b870963d485a836ba1
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-12 05:39:47 +0530

    feat: add check button function

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const billAmount = document.querySelector("#bill-amount");
+const cashAmount = document.querySelector("#cash-amount");
+const checkButton = document.querySelector("#check-button");
+
+const message = document.querySelector("#error-message");
+
+checkButton.addEventListener("click",()=>{
+    console.log("tested")
+});
+
