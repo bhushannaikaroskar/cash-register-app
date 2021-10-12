68ca50b513c16a63c9500d5363c733239571cebd
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-12 05:50:47 +0530

    feat: add input validation

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 
 checkButton.addEventListener("click",()=>{
-    console.log("tested")
+    hideMessage();
+    if(billAmount.value > 0){
+        if(cashAmount.value >= billAmount.value){
+            const change = cashAmount.value - billAmount.value;
+            calculateChange(change);
+        }else{
+            showMessage("Cash Amount should be greater than bill");
+        }
+    }else{
+        showMessage("Invalid Input");
+    }
 });
 
+function hideMessage(){
+    message.style.display = "none";
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerHTML = msg;
+}
+
+function calculateChange(amountToReturn){
+    console.log(amountToReturn);
+}
